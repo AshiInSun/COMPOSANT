ed972bb569e0c52bec6a06a900d58f148a2e33bb
AshiInSun <timorandeau@gmail.com>
2025-03-27 17:35:55 +0100

    Ajout du ConcreteEndpoint et Endpoint Composite en asynchrone

---
+++ src/defaultTeam/BCMAsyncContentNodeCompositeEndPoint.java
@@ -0,0 +1,43 @@
+package defaultTeam;
+import fr.sorbonne_u.components.AbstractPort;
+import fr.sorbonne_u.components.endpoints.BCMCompositeEndPoint;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.endpoints.ContentNodeBaseCompositeEndPointI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentAccessCI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentAccessSyncCI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.MapReduceCI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.MapReduceSyncCI;
+
+public class BCMAsyncContentNodeCompositeEndPoint extends BCMCompositeEndPoint 
+    	implements ContentNodeBaseCompositeEndPointI<ContentAccessCI, MapReduceCI> {
+
+    private static final long serialVersionUID = 1L;
+	protected static final int NUMBER_OF_ENDPOINTS = 2;
+
+    public BCMAsyncContentNodeCompositeEndPoint() {
+        super(NUMBER_OF_ENDPOINTS);
+ 
+        ConcreteAsyncBCMEndPoint<ContentAccessCI> contentAccessEndpoint =
+        		new ConcreteAsyncBCMEndPoint<ContentAccessCI>(
+                	ContentAccessCI.class,
+                	ContentAccessCI.class,
+                	AbstractPort.generatePortURI(ContentAccessCI.class));
+        this.addEndPoint(contentAccessEndpoint);
+        
+        ConcreteAsyncBCMEndPoint<MapReduceCI> mapReduceEndpoint =
+        		new ConcreteAsyncBCMEndPoint<MapReduceCI>(
+                    MapReduceCI.class,
+                    MapReduceCI.class,
+                    AbstractPort.generatePortURI(MapReduceCI.class));
+        this.addEndPoint(mapReduceEndpoint);
+    }
+
+    @Override
+    public ConcreteAsyncBCMEndPoint<ContentAccessCI> getContentAccessEndpoint() {
+        return (ConcreteAsyncBCMEndPoint<ContentAccessCI>) this.getEndPoint(ContentAccessCI.class);
+    }
+
+    @Override
+    public ConcreteAsyncBCMEndPoint<MapReduceCI> getMapReduceEndpoint() {
+        return (ConcreteAsyncBCMEndPoint<MapReduceCI>) this.getEndPoint(MapReduceCI.class);
+    }
+}
---
+++ src/defaultTeam/ConcreteAsyncBCMEndPoint.java
@@ -0,0 +1,68 @@
+package defaultTeam;
+import defaultTeam.port.DHTAsyncContentAccessInboundPort;
+import defaultTeam.port.DHTAsyncContentAccessOutboundPort;
+import defaultTeam.port.DHTAsyncMapReduceInboundPort;
+import defaultTeam.port.DHTAsyncMapReduceOutboundPort;
+import defaultTeam.port.DHTContentAccessConnector;
+import defaultTeam.port.DHTMapReduceConnector;
+import fr.sorbonne_u.components.AbstractComponent;
+import fr.sorbonne_u.components.AbstractPort;
+import fr.sorbonne_u.components.ports.AbstractInboundPort;
+import fr.sorbonne_u.components.endpoints.BCMEndPoint;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.content.ContentAccessCI;
+import fr.sorbonne_u.cps.dht_mapreduce.interfaces.mapreduce.MapReduceCI;
+
+
+public class ConcreteAsyncBCMEndPoint<CI extends fr.sorbonne_u.components.interfaces.RequiredCI>
+    extends BCMEndPoint<CI> {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ConcreteAsyncBCMEndPoint(Class<CI> implementedInterface,
+                               Class<? extends fr.sorbonne_u.components.interfaces.OfferedCI> serverSideOfferedInterface,
+                               String inboundPortURI) {
+        super(implementedInterface, serverSideOfferedInterface, inboundPortURI);
+    }
+
+    @Override
+    protected AbstractInboundPort makeInboundPort(AbstractComponent c, String inboundPortURI) throws Exception {
+		  if(this.getServerSideInterface().equals(ContentAccessCI.class)) {
+        	DHTAsyncContentAccessInboundPort inboundPort = new DHTAsyncContentAccessInboundPort(inboundPortURI, c);
+			inboundPort.publishPort();
+			return inboundPort;
+        	
+        
+        }else if(this.getServerSideInterface().equals(MapReduceCI.class)) {
+        	DHTAsyncMapReduceInboundPort inboundPort = new DHTAsyncMapReduceInboundPort(inboundPortURI, c);
+			inboundPort.publishPort();
+			return inboundPort;
+        	
+        }else {
+            throw new IllegalArgumentException("Interface serveur inconnue : " + this.getServerSideInterface());
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+	@Override
+	protected CI makeOutboundPort(AbstractComponent c, String inboundPortURI) throws Exception {
+    	
+		if(this.getClientSideInterface().equals(ContentAccessCI.class)) {
+        	String outboundPortURI = AbstractPort.generatePortURI(ContentAccessCI.class);
+            DHTAsyncContentAccessOutboundPort outboundPort = new DHTAsyncContentAccessOutboundPort(outboundPortURI, c);
+            outboundPort.publishPort();
+            DHTContentAccessConnector connector = new DHTContentAccessConnector();
+            outboundPort.doConnection(inboundPortURI, connector);
+            return (CI) outboundPort;
+            
+        }else if(this.getClientSideInterface().equals(MapReduceCI.class)) {
+        	String outboundPortURI = AbstractPort.generatePortURI(MapReduceCI.class);
+            DHTAsyncMapReduceOutboundPort outboundPort = new DHTAsyncMapReduceOutboundPort(outboundPortURI, c);
+            outboundPort.publishPort();
+            DHTMapReduceConnector connector = new DHTMapReduceConnector();
+            outboundPort.doConnection(inboundPortURI, connector);
+            return (CI) outboundPort;
+        }else {
+            throw new IllegalArgumentException("Interface client inconnue : " + this.getClientSideInterface());
+        }
+	}
+}
